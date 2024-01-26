cb6c353ddb871916cb6be231fa4f1ef3422102d4
Medikenji <nolanbijmholt@gmail.com>
2024-01-26 21:28:48 +0100

    Retry

--- Assets/Code/Nolan/HeadBob.cs
+++ Assets/Code/Nolan/HeadBob.cs
@@ -1,67 +1,119 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
 
-public class HeadBob : MonoBehaviour
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class Headbob : MonoBehaviour
+
 {
+
+    [Header("Configuration")]
+
     [SerializeField] private bool _enable = true;
-    [SerializeField, Range(0, 0.01f)] private float _amplitude = 0.015f;
-    [SerializeField, Range(0, 30)] private float _frequency = 10f;
-    [SerializeField] private  Transform _camera = null;
-    [SerializeField] private  Transform _cameraHolder = null;
+
+    [SerializeField, Range(0, 0.1f)] private float _Amplitude = 0.015f; [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
+
+    [SerializeField] private Transform _camera = null; [SerializeField] private Transform _cameraHolder = null;
+
+
+
     private float _toggleSpeed = 3.0f;
+
     private Vector3 _startPos;
+
     private CharacterController _controller;
 
-    void Start()
+
+
+    private void Awake()
+
     {
+
         _controller = GetComponent<CharacterController>();
+
         _startPos = _camera.localPosition;
+
     }
+
+
 
     void Update()
-    {
-if(!_enable) return;
 
-CheckMotion();
-ResetPosition();
-_camera.LookAt(FocusTarget());
+    {
+
+        if (!_enable) return;
+
+        CheckMotion();
+
+        ResetPosition();
+
+        _camera.LookAt(FocusTarget());
 
     }
 
-private void CheckMotion()
-{
-    float speed = new Vector3(_controller.velocity.x, 0, _controller.velocity.z).magnitude;
+    private Vector3 FootStepMotion()
 
-    if(speed < _toggleSpeed) return;
-    if (!_controller.isGrounded) return;
-    PlayMotion(Footstepmotion());
-}
-
-    private Vector3 Footstepmotion()
     {
+
         Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(Time.time * _frequency) * _amplitude;
-        pos.x += Mathf.Sin(Time.time * _frequency) * _amplitude;
+
+        pos.y += Mathf.Sin(Time.time * _frequency) * _Amplitude;
+
+        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _Amplitude * 2;
+
         return pos;
+
     }
 
-   private void ResetPosition()
-   {
-    if(_camera.localPosition == _startPos) return;
-    _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
-   }
+    private void CheckMotion()
 
-private Vector3 FocusTarget()
-{
-    Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
-    pos += _cameraHolder.forward * 15.0f;
-    return pos;
-}
+    {
+
+        float speed = new Vector3(_controller.velocity.x, 0, _controller.velocity.z).magnitude;
+
+        if (speed < _toggleSpeed) return;
+
+        if (!_controller.isGrounded) return;
 
 
-private void PlayMotion(Vector3 motion){
-_camera.localPosition += motion; 
-}
+
+        PlayMotion(FootStepMotion());
+
+    }
+
+    private void PlayMotion(Vector3 motion)
+
+    {
+
+        _camera.localPosition += motion;
+
+    }
+
+
+
+    private Vector3 FocusTarget()
+
+    {
+
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
+
+        pos += _cameraHolder.forward * 15.0f;
+
+        return pos;
+
+    }
+
+    private void ResetPosition()
+
+    {
+
+        if (_camera.localPosition == _startPos) return;
+
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
+
+    }
+
 }
